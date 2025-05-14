b36e1fe89472ed6e10ee2d75e347ffcea49ac292
nipunaRajapaksha <nshalitha7@gmail.com>
2025-05-14 14:43:23 +0200

    git commit -m "chore: Initial Java Spring Boot 3.4.5 project, added dependencies: Spring Web, Spring Data JPA, H2, Validation, Lombok, Spring Boot Test"

---
+++ src/test/java/com/nipuna/tractive/pettracker/PetTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nipuna.tractive.pettracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
